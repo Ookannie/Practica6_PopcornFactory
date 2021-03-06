3e1984e1ba5987de55bb31f2b95e3339ff0c2ac6
Ana <ana.gamez204360@potros.itson.edu.mx>
2021-03-05 23:55:13 -0700

    Plantillas

--- app/src/main/java/gamez/ana/popcornfactory/MainActivity.kt
+++ app/src/main/java/gamez/ana/popcornfactory/MainActivity.kt
@@ -2,10 +2,27 @@ package gamez.ana.popcornfactory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+import kotlinx.android.synthetic.main.pelicula.view.*
 
 class MainActivity : AppCompatActivity() {
+
+    var adapter: PeliculaAdapter? = null
+    var peliculas = ArrayList<Pelicula>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cargarPeliculas()
+
+        adapter = PeliculaAdapter(this, peliculas)
+        gridView.adapter = adapter
+    }
+
+    fun cargarPeliculas(){
+        peliculas.add(Pelicula("Bones", R.drawable.bones, R.drawable.bonesheader, R.string.desc_bones.toString()))
     }
 }
+
