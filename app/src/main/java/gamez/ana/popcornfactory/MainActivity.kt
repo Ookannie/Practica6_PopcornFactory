c5898a5e722560f4f132cbf552491ea48ba9d9f6
Ana <ana.gamez204360@potros.itson.edu.mx>
2021-03-06 19:10:17 -0700

    lil fix

--- app/src/main/java/gamez/ana/popcornfactory/MainActivity.kt
+++ app/src/main/java/gamez/ana/popcornfactory/MainActivity.kt
@@ -22,15 +22,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun cargarPeliculas(){
-        peliculas.add(Pelicula("Bones", R.drawable.bones, R.drawable.bonesheader, getString(R.string.desc_bones)))
-        peliculas.add(Pelicula("Dr.House", R.drawable.drhouse, R.drawable.drwhoheader, getString(R.string.desc_house)))
-        peliculas.add(Pelicula("Big Hero 6", R.drawable.bighero6, R.drawable.headerbighero6,getString( R.string.desc_hero)))
-        peliculas.add(Pelicula("Dr. Who", R.drawable.drwho, R.drawable.drwhoheader,getString( R.string.desc_who)))
-        peliculas.add(Pelicula("Friends", R.drawable.friends, R.drawable.friendsheader,getString (R.string.desc_friends)))
-        peliculas.add(Pelicula("Inception", R.drawable.inception, R.drawable.inceptionheader,getString (R.string.desc_inception)))
-        peliculas.add(Pelicula("Leap Year", R.drawable.leapyear, R.drawable.leapyearheader, getString(R.string.desc_leap)))
-        peliculas.add(Pelicula("Toy Story", R.drawable.toystory, R.drawable.toystoryheader, getString(R.string.desc_toy)))
-        peliculas.add(Pelicula("Smallville", R.drawable.smallville, R.drawable.smallvilleheader, getString(R.string.desc_small)))
+        peliculas.add(Pelicula(getString(R.string.titulo_bones), R.drawable.bones, R.drawable.bonesheader, getString(R.string.desc_bones)))
+        peliculas.add(Pelicula(getString(R.string.titulo_drhouse), R.drawable.drhouse, R.drawable.drwhoheader, getString(R.string.desc_house)))
+        peliculas.add(Pelicula(getString(R.string.titulo_hero), R.drawable.bighero6, R.drawable.headerbighero6,getString( R.string.desc_hero)))
+        peliculas.add(Pelicula(getString(R.string.titulo_who), R.drawable.drwho, R.drawable.drwhoheader,getString( R.string.desc_who)))
+        peliculas.add(Pelicula(getString(R.string.titulo_friends), R.drawable.friends, R.drawable.friendsheader,getString (R.string.desc_friends)))
+        peliculas.add(Pelicula(getString(R.string.titulo_inception), R.drawable.inception, R.drawable.inceptionheader,getString (R.string.desc_inception)))
+        peliculas.add(Pelicula(getString(R.string.titulo_leap), R.drawable.leapyear, R.drawable.leapyearheader, getString(R.string.desc_leap)))
+        peliculas.add(Pelicula(getString(R.string.titulo_toy), R.drawable.toystory, R.drawable.toystoryheader, getString(R.string.desc_toy)))
+        peliculas.add(Pelicula(getString(R.string.titulo_small), R.drawable.smallville, R.drawable.smallvilleheader, getString(R.string.desc_small)))
     }
 }
 
