48e2dc0d1faa637ebe5982e508c0d327b48e08f9
Ana <ana.gamez204360@potros.itson.edu.mx>
2021-03-07 16:39:55 -0700

    lil fix vistas

--- app/src/main/java/gamez/ana/popcornfactory/ListaPeliculasActivity.kt
+++ app/src/main/java/gamez/ana/popcornfactory/ListaPeliculasActivity.kt
@@ -2,6 +2,7 @@ package gamez.ana.popcornfactory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridView
 import kotlinx.android.synthetic.main.activity_lista_peliculas.*
 
 
@@ -11,12 +12,11 @@ class ListaPeliculasActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_lista_peliculas)
         cargarPeliculas()
 
         adapter = PeliculaAdapter(this, peliculas)
-        gridView.adapter = adapter
+         gridview.adapter = adapter
     }
 
     fun cargarPeliculas(){
--- app/src/main/java/gamez/ana/popcornfactory/MainActivity.kt
+++ app/src/main/java/gamez/ana/popcornfactory/MainActivity.kt
@@ -1,10 +1,10 @@
 package gamez.ana.popcornfactory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Button
 
-import kotlinx.android.synthetic.main.pelicula.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button:Button = findViewById(R.id.button_get_started) as Button
+
+        button.setOnClickListener {
+            var intent: Intent = Intent(this, ListaPeliculasActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
