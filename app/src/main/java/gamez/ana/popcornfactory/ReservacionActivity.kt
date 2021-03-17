59c2972ed2025fa54eb17006a61afe36118838ed
Ana <ana.gamez204360@potros.itson.edu.mx>
2021-03-17 00:15:59 -0700

    weird

--- app/src/main/java/gamez/ana/popcornfactory/ReservacionActivity.kt
+++ app/src/main/java/gamez/ana/popcornfactory/ReservacionActivity.kt
@@ -2,10 +2,20 @@ package gamez.ana.popcornfactory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ReservacionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservacion)
+
+        val title: TextView = findViewById(R.id.movie)
+        val numSeat: TextView = findViewById(R.id.numSeat)
+        val bundle = intent.extras
+
+        if (bundle!= null){
+            title.setText("Seat number" +bundle.getString("movie"))
+            numSeat.setText(bundle.getInt("seat").toString())
+        }
     }
 }
